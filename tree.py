0c948812a31679c4b51d2d75dda8566a4293a29c
pontushojer <pontushojer@gmail.com>
2017-04-08 13:03:36 +0200

    added tree solution

---
+++ tree.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Problem: Completing a Tree
+url: http://rosalind.info/problems/tree/
+
+Given: A positive integer nn (n≤1000n≤1000) and an adjacency list corresponding to a graph on nn nodes that contains no cycles.
+
+Return: The minimum number of edges that can be added to the graph to produce a tree.
+"""
+
